824febdf5a35f56f71539b16b9cf464cf030d622
Kai Zhang <6542131+zhangks98@users.noreply.github.com>
2023-12-03 14:18:04 +0100

    [nvim] Use Tab for snippet nav.

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -49,6 +49,7 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.shortmess:append("c")
 
 local lspkind = require("lspkind")
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 
 cmp.setup({
@@ -58,6 +59,24 @@ cmp.setup({
     ["<C-e>"] = cmp.mapping.close(),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
 
   sources = {
@@ -71,7 +90,7 @@ cmp.setup({
 
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
 
